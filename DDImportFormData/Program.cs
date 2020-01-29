6c08577347b191eeaef26913a2ac3390db43ff08
Kuni <kuni.ujiie@grapecity.com>
2020-01-29 16:37:40 +0900

    プロジェクト ファイルを追加します。

---
+++ DDImportFormData/Program.cs
@@ -0,0 +1,39 @@
+﻿using GrapeCity.Documents.Pdf;
+using System;
+using System.IO;
+
+namespace DDImportFormData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DioDocs for PDF. PDF Form Data Import!");
+
+            var doc = new GcPdfDocument();
+
+            // Load the document
+            doc.Load(new FileStream("grapecity_order_template.pdf", FileMode.Open, FileAccess.Read));
+
+            // FDF
+            // Open the FDF file
+            //FileStream stream = new FileStream("FormData_FDF.fdf", FileMode.Open, FileAccess.Read);
+            // Import the form data
+            //doc.ImportFormDataFromFDF(stream);  
+
+            // XFDF
+            // Open the XFDF file
+            FileStream stream = new FileStream("FormData_XFDF.xfdf", FileMode.Open, FileAccess.Read);
+            // Import the form data 
+            doc.ImportFormDataFromXFDF(stream);       
+
+            // XML
+            //Open the XML file
+            //FileStream stream = new FileStream("FormData_XML.xml", FileMode.Open, FileAccess.Read);
+            //Import the form data
+            //doc.ImportFormDataFromXML(stream);        
+
+            doc.Save("grapecity_order_embed.pdf"); //Save the document
+        }
+    }
+}
